69a86397fb948bb51cdd9ebd1885cfb922326305
Kim Seungha <seungha.me@gmail.com>
2018-06-19 16:34:12 +0900

    Redirect 컴포넌트로 페이지 전환

--- src/App.js
+++ src/App.js
@@ -1,27 +1,39 @@
-import React, { Component } from 'react'; // 노드 모듈스 안에 라이브러리 모듈을 쓸 떄는 경로를 안쓴다.
+import React, { Component } from 'react';
+import {BrowserRouter, Route, Redirect} from 'react-router-dom';
 
 import TodoPage from './pages/TodoPage'
 import LoginPage from './pages/LoginPage'
 
-import {PageProvider, PageConsumer} from './contexts/PageContext'
 import {UserProvider} from './contexts/UserContext';
 
 class App extends Component {
   render() {
     return (
-      <PageProvider>
+      <BrowserRouter>
         <UserProvider>
-          <PageConsumer>
-            {value => value.page === 'login' ? (
-              <LoginPage />
-            ) : value.page === 'todo' ? (
-              <TodoPage />
-            ) : null}
-          </PageConsumer>
+          <Route path="/login" component={LoginPage} />
+          <Route path="/todo" component={TodoPage} />
+          <Route exact path="/" component={Home} />
         </UserProvider>
-      </PageProvider>
+      </BrowserRouter>
     )
   }
 }
+
+const Home = () => {
+  if (localStorage.getItem('token')) {
+    return <Redirect to="/todo" />;
+  } else {
+    return <Redirect to="/login" />;
+  }
+}
+
+// const Home = () => (
+//   localStorage.getItem('token') ? (
+//     <Redirect to="/todo" />
+//   ) : (
+//     <Redirect to="/login" />
+//   )
+// )
 
 export default App;
